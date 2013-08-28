b1f37764f48a6838d729dfaebef343648e7be28e
Joshua Tenner <Tenner.joshua@gmail.com>
2013-08-28 12:33:46 -0400

    Update bugfixes for % operator

--- expando.alt.js
+++ expando.alt.js
@@ -4,7 +4,10 @@ var expandoalt = function(){
         cls = /\.([a-z][a-z\-\_0-9]*)/gi,
         tag = /^[\s]*([a-z]*)/i,
         cnt = /\*([0-9]*)/,
-		prms = / /,
+		prms = /\[((?:\\\]|[^\]])*)\]/gm,
+		t = /[^\\]%([a-z][a-z0-9\_\.]*)/gmi,
+		lit = /^\=(.*)\}/,
+		newline = /(\r\n|\n|\r)/gm,
 	repeat = function (str, times) {
 		var res = '';
 		while (times > 0) {
@@ -29,8 +32,8 @@ var expandoalt = function(){
 	},
 	EM = function(n, strict, tabs) {
 		var noEndTag = false, read;
-		if(read = /^\=(.*)\}/.exec(n.expansion)){
-			return read[1];
+		if(read = lit.exec(n.expansion)){
+			return read[1].trim();
 		}
 		tabs = tabs || 0;
 		switch (n.tag.toLowerCase()) {
@@ -46,7 +49,6 @@ var expandoalt = function(){
 		  case "input":
 		  case "keygen":
 		  case "embed":
-		  case "keygen":
 		  case "param":
 		  case "source":
 		  case "track":
@@ -87,6 +89,7 @@ var expandoalt = function(){
 					push.apply(nodelist[index].children, lexf(instructions));
 					break;
 				case "}":
+					console.log(nodelist);
 					return nodelist;
 				case "\\":
 					if(read = instructions[0]){
@@ -109,24 +112,28 @@ var expandoalt = function(){
 		return res;
 	},
 	expando = function(expansion, obj){
+		expansion = " " + replace.call(expansion, newline," ");
 		if(obj){
-			var t = /\%([a-z][a-z0-9\_\.]*)/gmi, read;
+			var read;
 			expansion = replace.call(expansion,t,function(match, $1){
 				var nested = $1.split('.').reverse(), val = obj;
 				while(read = nested.pop()){
 					val = val[read];
 				}
-				return val;
+				return match[0]+val;
 			});
 		}
 		return parse(lexf(expansion.match(lex)))||"<div></div>";		
 	};
 	node.prototype.expand = function() {
-        var x = this.expansion;		
+        var x = this.expansion, that = this;		
 		this.id = ((x.match(id) ? x.match(id)[1] : this.id) || this.id).trim();
 		this.tag = ((x.match(tag) ? x.match(tag)[0] : this.tag) || this.tag).trim();
 		this.classList = x.match(cls) || [];
 		this.count = x.match(cnt) ? parseInt(x.match(cnt)[1]) : 1;
+		replace.call(x, prms, function(match, $1){
+			that.modifiers += $1 + " ";
+		});
 		if (this.children.length) for (var i = 0; i < this.children.length; i++) {
 			this.children[i].expand();
 		}
